06c73f214b4fc045e436911059d5a87aa83da754
wangmingliang <rszswang@126.com>
2018-01-31 10:12:21 +0800

    add feign to project

---
+++ feign/src/main/java/com/cloud/feign/api/SchedualServiceHi.java
@@ -0,0 +1,14 @@
+package com.cloud.feign.api;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "eureka-client")//通过@ FeignClient（“服务名”），来指定调用哪个服务
+public interface SchedualServiceHi {
+
+	@RequestMapping(value = "/hi", method = RequestMethod.GET)
+	// 要为@RequestParam指定value不然启动报错"java.lang.IllegalStateException: RequestParam.value() was empty on parameter 0"
+	String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
